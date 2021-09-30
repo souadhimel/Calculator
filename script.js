529f66edc7e30cd7ea5855eb53494d19d91fdd16
Souad Himel <souad.anam@gmail.com>
2021-09-30 19:22:54 +0600

    text

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function deleteMe() {}
+function deleteMe() {
+  document.getElementById("myResult").value = "";
+}
 function calculator(newValue) {
   document.getElementById("myResult").value += newValue;
 }
-function answer() {}
+function answer() {
+  let a = document.getElementById("myResult").value;
+  let b = eval(a);
+  document.getElementById("myResult").value = b;
+}
